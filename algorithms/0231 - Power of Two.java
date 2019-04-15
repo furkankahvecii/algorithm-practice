b570878e6e306c1c793669eab495f05465fe15a4
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-04-15 22:03:37 +0300

    Create 0231 - Power of Two.java

---
+++ algorithms/0231 - Power of Two.java
@@ -0,0 +1,17 @@
+/*
+* https://leetcode.com/problems/power-of-two/
+*/
+
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0 || n<0) return false;
+        while(n>1)
+		 {
+			if(n%2 == 1) return false;
+			n=n/2;
+		 }
+		 return true;
+    }
+}
