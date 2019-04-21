017c17ab272cd42415e4ba372ae114474ad35337
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-04-21 23:43:15 +0300

    Create 0509 -  Fibonacci Number.java

---
+++ algorithms/0509 -  Fibonacci Number.java
@@ -0,0 +1,14 @@
+/*
+* https://leetcode.com/problems/fibonacci-number/
+*/
+
+
+
+class Solution {
+    public int fib(int N) 
+    {
+       if(N<=1) return N;
+	        else
+	            return fib(N-1) + fib(N-2);        
+    }
+}
