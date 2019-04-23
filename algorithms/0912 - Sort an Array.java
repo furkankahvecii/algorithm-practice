c3b4bf34d5a69d93a83198e7f0ad4fe02ed227ff
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-04-23 04:25:40 +0300

    Create 0912 - Sort an Array.java

---
+++ algorithms/0912 - Sort an Array.java
@@ -0,0 +1,14 @@
+/*
+* https://leetcode.com/problems/sort-an-array/
+*/
+
+
+
+import java.util.*;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+         Arrays.sort(nums);
+	        return nums;
+    }
+}
