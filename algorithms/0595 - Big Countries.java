7e12fe14683c13c26057a0b0afa9b2f0148c74d8
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-05-11 03:04:56 +0300

    Update 0595 - Big Countries.java

--- algorithms/0595 - Big Countries.java
+++ algorithms/0595 - Big Countries.java
@@ -1,4 +1,9 @@
-# Write your MySQL query statement below
+/*
+* https://leetcode.com/problems/big-countries/
+*/
+
+
+
 SELECT World.name, World.population, World.area 
 FROM World 
 WHERE World.population>25000000 OR World.area>3000000;
