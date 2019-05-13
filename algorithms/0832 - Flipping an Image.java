e0f6dc3524bbeb71779ba4af1cab55d9ba8e4c1a
Furkan Kahveci <33764493+furkankahvecii@users.noreply.github.com>
2019-05-13 23:45:39 +0300

    Create 0832 - Flipping an Image.java

---
+++ algorithms/0832 - Flipping an Image.java
@@ -0,0 +1,21 @@
+/*
+* https://leetcode.com/problems/flipping-an-image/
+*/
+
+
+
+class Solution 
+{
+	public static int[][] flipAndInvertImage(int[][] A) 
+	{
+		int[][] result = new int[A.length][A[0].length];
+		for(int i=0;i<A.length;i++)
+		{
+			for(int j=A[i].length-1;j>=0;j--)
+			{
+				result[i][A[i].length-1-j] = A[i][j] > 0 ? 0:1;
+			}
+		}
+		return result;
+	}
+}
