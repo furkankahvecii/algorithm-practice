feeec6e112fd8141682f7fc1eeb0563f212fab4a
Furkan Kahveci <furkankahvecii@hotmail.com.tr>
2019-08-28 10:23:02 +0300

    Create 0205 - Isomorphic Strings.java

---
+++ algorithms/0205 - Isomorphic Strings.java
@@ -0,0 +1,27 @@
+/*
+* https://leetcode.com/problems/isomorphic-strings/
+*/
+
+
+
+import java.util.*;
+
+class Solution 
+{
+	public boolean isIsomorphic(String s, String t) 
+	{
+        Map<Character,Character> map = new HashMap<Character,Character>();
+		
+		for(int i=0;i<s.length();i++)
+		{
+			if(map.containsKey(s.charAt(i))  &&  map.get(s.charAt(i)) != t.charAt(i))
+				return false;
+			if(!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i)))
+				return false;
+			
+			map.put(s.charAt(i),t.charAt(i));
+		}
+		
+		return true;
+    }
+}
