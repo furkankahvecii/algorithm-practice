c4fed2af4ba0f2c69ac9d426406e1c99d30587c2
Furkan Kahveci <furkankahvecii@hotmail.com.tr>
2019-11-22 01:19:42 +0300

    Create 1089 - Duplicate Zeros.java

---
+++ algorithms/1089 - Duplicate Zeros.java
@@ -0,0 +1,23 @@
+/*
+ * https://leetcode.com/problems/duplicate-zeros/
+ */
+
+
+
+public class Solution {
+    public static void duplicateZeros(int[] arr) 
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+        	if(arr[i] == 0)
+        	{
+        		for(int j=arr.length-1;j>i+1;j--)
+        		{
+        			arr[j] = arr[j-1];
+        		}
+        		arr[i+1] = 0;
+        		i++;
+        	}
+        }   
+    } 
+}
