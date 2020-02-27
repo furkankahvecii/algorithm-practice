71cfe7ae679c69ccf0c514cfcb6b385f821bad6b
furkankahvecii <furkankahveci@hotmail.com.tr>
2020-02-27 22:12:47 +0300

    Create 1309 - Decrypt String from Alphabet to Integer Mapping.java

---
+++ algorithms/1309 - Decrypt String from Alphabet to Integer Mapping.java
@@ -0,0 +1,25 @@
+/*
+ * https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+*/
+
+
+
+class Solution {
+	public String freqAlphabets(String s) {
+     
+        StringBuilder str = new StringBuilder("");
+    	for(int i=s.length()-1;i>=0;i--)
+    	{
+    		if(s.charAt(i) == '#') {
+                str.append((char)('a' + (s.charAt(i-1) -'0' ) + 10*(s.charAt(i-2)-'0') -1) ); 
+    			i-=2;
+    		}
+    		else {
+    			 str.append((char)('a' + (s.charAt(i) - '0') -1));
+    		}
+    	}
+    	
+    	str.reverse();
+    	return str.toString();
+    }
+}
